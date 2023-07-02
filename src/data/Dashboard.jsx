117c7bde25265f6d44957cb70432ecc18fecbde5
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-03 00:38:00 +0700

    data for users, drivers, and dashboard

---
+++ src/data/Dashboard.jsx
@@ -0,0 +1,70 @@
+import { BiSolidBusSchool } from "react-icons/bi";
+import { RiLogoutBoxRFill } from "react-icons/ri";
+import { TbMoodKidFilled } from "react-icons/tb";
+import { FaAddressCard, FaSchool, FaUserAlt, FaHistory } from "react-icons/fa";
+import {
+  FaMapLocationDot,
+  FaMapLocation,
+  FaLocationDot,
+} from "react-icons/fa6";
+import { users } from "../data/Users";
+import { drivers } from "../data/Drivers";
+
+export const data_dashboard = [
+  {
+    id: "0",
+    name: "Tracking Position",
+    link: "/dashboard_user",
+    icon: FaLocationDot,
+  },
+  {
+    id: "1",
+    name: "ID Kartu",
+    desc: users.find((user) => user.id === "1")?.card_id,
+    icon: FaAddressCard,
+  },
+  {
+    id: "2",
+    name: "Nama Anak",
+    desc: users.find((user) => user.id === "1")?.name,
+    icon: TbMoodKidFilled,
+  },
+  {
+    id: "3",
+    name: "Sekolah",
+    desc: users.find((user) => user.id === "1")?.school,
+    icon: FaSchool,
+  },
+  {
+    id: "4",
+    name: "Posisi Naik",
+    desc: "Politeknik Negri Jakarta",
+    icon: FaMapLocationDot,
+  },
+  {
+    id: "5",
+    name: "Posisi Turun",
+    desc: "Halte Universitas Indonesia",
+    icon: FaMapLocation,
+  },
+  {
+    id: "6",
+    name: "Bus Info",
+    desc: drivers.find((user) => user.id === "1")?.buses,
+    icon: BiSolidBusSchool,
+  },
+  {
+    id: "7",
+    name: "Driver",
+    desc: drivers.find((user) => user.id === "1")?.name,
+    icon: FaUserAlt,
+  },
+  {
+    id: "8",
+    name: "History",
+    link: "/dashboard_user/history",
+    icon: FaHistory,
+    margin: true,
+  },
+  { id: "9", name: "Logout", link: "/", icon: RiLogoutBoxRFill, margin: true },
+];
---
+++ src/data/Users.jsx
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    id: "1",
+    name: "Jonni Simbolon",
+    card_id: "10A98DB321",
+    school: "Politeknik Negri jakarta",
+    parent: "Jahmal",
+  },
+  {
+    id: "2",
+    name: "Udut De Kantal",
+    card_id: "05A89EX561",
+    school: "Politeknik Negri jakarta",
+    parent: "Asep Mahal",
+  },
+  {
+    id: "3",
+    name: "Hasbullah Rahmat",
+    card_id: "A2G25TE456",
+    school: "Politeknik Negri jakarta",
+    parent: "Nurhaman MJ",
+  },
+];
