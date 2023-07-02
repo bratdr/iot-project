b4b0fdb0394fd89c2fcdec7218de374f34813b15
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-03 00:45:45 +0700

    change the history navigation

--- src/pages/LoginUser/History/index.jsx
+++ src/pages/LoginUser/History/index.jsx
@@ -1,13 +1,13 @@
-import Navigation from "./HistoryNav";
+import Navigation from "../../../components/HistoryNav";
 
 const History = () => {
   return (
     <>
-      <div className="flex flex-row w-screen min-h-screen">
-        <div className="z-10">
+      <div className="z-0 flex flex-row w-screen min-h-screen">
+        <div className="z-10 absolute">
           <Navigation />
         </div>
-        <div className="z-0 flex bg-white rounded-md w-screen mx-4 items-center justify-center">
+        <div className="flex bg-slate-100 rounded-md w-screen items-center justify-center">
           <h1>History</h1>
         </div>
       </div>
