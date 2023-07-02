a28cbfbc3dcbfa930b2be71440e8810c9fb35d64
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-03 00:37:20 +0700

    user search

---
+++ src/components/UserSearch.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+const UserSearch = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex items-center">
+        <div className="flex border border-slate-200">
+          <input
+            type="text"
+            className="block w-60  px-4 py-2 text-slate-700 bg-white border focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            placeholder="Tracking Childern Position"
+          />
+          <button
+            onClick={() => navigate("/dashboard_user")}
+            className=" text-white bg-black border-l rounded-none hover:bg-slate-900"
+          >
+            Find
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserSearch;
