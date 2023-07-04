648dd0eb0114efc4d8d04e34d5332186c24093ef
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-04 16:46:30 +0700

    update route in onClick navigate

--- src/pages/LoginAdmin/index.jsx
+++ src/pages/LoginAdmin/index.jsx
@@ -1,28 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import UserSearch from "../../components/AdminSearch";
 import adminGif from "../../assets/manage-admin.gif";
 
 const AdminLogin = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="bg-black text-white  flex flex-col text-center gap-4 px-4 items-center justify-center w-screen h-screen">
-        <h1 className="font-bold text-3xl">
+      <div className="flex h-screen  w-screen flex-col items-center justify-center gap-4 px-4 text-center text-black">
+        <h1 className="text-3xl font-bold">
           Manage Your Tracking
           <br />
           Application Here
         </h1>
         <p className="pb-6">Insert Your Admin Key For Login!</p>
         <img className="max-w-xs" src={adminGif} alt="my-gif" />
-        <div className="pt-6">
-          <UserSearch />
+        <div className="flex flex-row items-center justify-center gap-2">
+          <button
+            onClick={() => navigate("/admin/dashboard")}
+            className="mt-12 flex w-24 items-center justify-center rounded-lg bg-black text-sm font-medium text-white hover:bg-rose-500 hover:font-bold hover:text-white"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => navigate("/")}
+            className="mt-12 flex w-24 items-center justify-center rounded-lg border border-black bg-transparent text-sm font-medium text-black hover:bg-rose-500 hover:font-bold hover:text-white"
+          >
+            Back
+          </button>
         </div>
-        <button
-          onClick={() => navigate("/")}
-          className="text-sm mt-4 bg-black text-white w-18 font-medium flex items-center justify-center border-rose-500 border rounded-full hover:bg-rose-500"
-        >
-          Back
-        </button>
       </div>
     </>
   );
