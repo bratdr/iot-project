851b1b9aed45fde0cbc5981e8ac7cfca4c13e20c
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-04 16:55:05 +0700

    update navigation width

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,25 +18,25 @@ const Navigation = () => {
       <div>
         <section className="flex gap-2">
           <div
-            className={`bg-[#000] min-h-screen border border-l-0 border-y-0 border-black ${
-              open ? "w-screen sm:w-screen bg-opacity-95" : "w-16"
-            } duration-1000 text-gray-100 px-4`}
+            className={`min-h-screen border border-y-0 border-l-0 border-black bg-[#000] ${
+              open ? "w-72 bg-opacity-95 sm:w-80" : "w-16"
+            } px-4 text-gray-100 duration-1000`}
           >
-            <div className="py-3 flex justify-end">
+            <div className="flex justify-end py-3">
               <HiMenuAlt3
                 size={26}
                 className="cursor-pointer text-white"
                 onClick={() => setOpen(!open)}
               />
             </div>
-            <div className="mt-4 flex flex-col gap-4 relative">
+            <div className="relative mt-4 flex flex-col gap-4">
               {data_dashboard?.map((data_ds, i) => (
                 <Link
                   to={data_ds?.link}
                   key={i}
                   className={` ${
                     data_ds?.margin && "mt-4"
-                  } group flex items-center text-sm gap-3.5 font-medium p-2 text-white hover:text-rose-600 hover:bg-slate-100 rounded-md`}
+                  } group flex items-center gap-3.5 rounded-md p-2 text-sm font-medium text-white hover:bg-slate-100 hover:text-rose-600`}
                   onClick={() => setOpen(!open)}
                 >
                   <div>
@@ -48,7 +48,7 @@ const Navigation = () => {
                         transitionDelay: `${i + 3}00ms`,
                       }}
                       className={`whitespace-pre text-rose-500 duration-500 ${
-                        !open && "opacity-0 translate-x-28 overflow-hidden"
+                        !open && "translate-x-28 overflow-hidden opacity-0"
                       }`}
                     >
                       {data_ds?.name}
@@ -56,7 +56,7 @@ const Navigation = () => {
                     <h2
                       className={`${
                         open && "hidden"
-                      } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+                      } absolute left-48 w-0 overflow-hidden whitespace-pre rounded-md bg-white px-0 py-0 font-semibold text-gray-900 drop-shadow-lg group-hover:left-14 group-hover:w-fit group-hover:px-2 group-hover:py-1 group-hover:duration-300  `}
                     >
                       {data_ds?.name}
                     </h2>
@@ -65,7 +65,7 @@ const Navigation = () => {
                         transitionDelay: `${i + 3}00ms`,
                       }}
                       className={`whitespace-pre text-sm text-indigo-500 duration-500 ${
-                        !open && "opacity-0 translate-x-28 overflow-hidden"
+                        !open && "translate-x-28 overflow-hidden opacity-0"
                       }`}
                     >
                       {data_ds?.desc}
@@ -73,7 +73,7 @@ const Navigation = () => {
                     <p
                       className={`${
                         open && "hidden"
-                      } absolute left-48 bg-white font-medium whitespace-pre text-sm text-gray-900  rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+                      } absolute left-48 w-0 overflow-hidden whitespace-pre rounded-md bg-white  px-0 py-0 text-sm font-medium text-gray-900 drop-shadow-lg group-hover:left-14 group-hover:w-fit group-hover:px-2 group-hover:py-1 group-hover:duration-300  `}
                     >
                       {data_ds?.desc}
                     </p>
