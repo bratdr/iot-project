367f61565116c49d34e89947774a1e1e1985d27c
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-04 16:44:16 +0700

    update route in onClick navigate

--- src/components/SelectAccount.jsx
+++ src/components/SelectAccount.jsx
@@ -7,20 +7,20 @@ const SelectAccount = () => {
 
   return (
     <>
-      <div className="w-screen flex flex-col items-center justify-center px-4">
-        <h1 className="font-bold text-3xl">Select Account</h1>
+      <div className="flex w-screen flex-col items-center justify-center px-4">
+        <h1 className="text-3xl font-bold">Select Account</h1>
         <p className="pb-6">Choose your account type!</p>
         <div className="flex gap-3">
           <div
-            onClick={() => navigate("/user_login")}
-            className="flex flex-col gap-7 py-6 px-4 w-1/2 justify-center items-center outline-slate-300 outline outline-1 overflow-hidden hover:outline-slate-700"
+            onClick={() => navigate("/login/user")}
+            className="flex w-1/2 flex-col items-center justify-center gap-7 overflow-hidden px-4 py-6 outline outline-1 outline-slate-300 hover:outline-slate-700"
           >
             <img src={userGif} alt="my-gif" />
             <h1 className="text-xl font-bold">User</h1>
           </div>
           <div
-            onClick={() => navigate("/admin_login")}
-            className="flex flex-col gap-7 py-6 px-4 w-1/2 justify-center items-center outline-slate-300 outline outline-1 overflow-hidden hover:outline-slate-700"
+            onClick={() => navigate("/login/admin")}
+            className="flex w-1/2 flex-col items-center justify-center gap-7 overflow-hidden px-4 py-6 outline outline-1 outline-slate-300 hover:outline-slate-700"
           >
             <img src={adminGif} alt="my-gif" />
             <h1 className="text-xl font-bold">Admin</h1>
