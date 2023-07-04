1c98761a63143923c63759831b0e8e0bf24f07a8
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-04 16:43:15 +0700

    update route in onClick navigate

--- src/components/AdminSearch.jsx
+++ src/components/AdminSearch.jsx
@@ -8,12 +8,12 @@ const AdminSearch = () => {
         <div className="flex border border-slate-200">
           <input
             type="text"
-            className="block w-60  px-4 py-2 text-slate-700 bg-white border focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            className="block w-60  border bg-white px-4 py-2 text-slate-700 focus:border-slate-400 focus:outline-none focus:ring focus:ring-slate-300 focus:ring-opacity-40"
             placeholder="Input the admin key..."
           />
           <button
-            onClick={() => navigate("/dashboard_admin")}
-            className=" text-black bg-rose-500 border-l rounded-none hover:bg-black hover:text-white"
+            onClick={() => navigate("/admin/dashboard")}
+            className=" rounded-none border-l bg-black text-white hover:bg-rose-500 hover:text-black"
           >
             Enter
           </button>
--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -8,12 +8,12 @@ const UserSearch = () => {
         <div className="flex border border-slate-200">
           <input
             type="text"
-            className="block w-60  px-4 py-2 text-slate-700 bg-white border focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            className="block w-60 rounded-none border bg-white px-4 py-2 text-slate-700 focus:border-slate-400 focus:outline-none focus:ring focus:ring-slate-300 focus:ring-opacity-40"
             placeholder="Tracking Childern Position"
           />
           <button
-            onClick={() => navigate("/dashboard_user")}
-            className=" text-white bg-black border-l rounded-none hover:bg-rose-500 hover:text-black"
+            onClick={() => navigate("/user/dashboard")}
+            className=" rounded-none border-l bg-black text-white hover:bg-rose-500 hover:text-black"
           >
             Find
           </button>
