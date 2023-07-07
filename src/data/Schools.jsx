02561d1cbe8cfd2cc316d502d98bf25743c56ac6
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 02:00:19 +0700

    added data lists for school

---
+++ src/data/Schools.jsx
@@ -0,0 +1,18 @@
+export const schools = [
+  {
+    id: "1",
+    name: "Universitas Indonesia",
+    alamat: "Pondok Cina, Beji, Depok City, West Java 16424",
+  },
+  {
+    id: "2",
+    name: "Universitas Gunadarma",
+    alamat: "Jl. Margonda Raya 100, Depok City, West Java 16424 ",
+  },
+  {
+    id: "3",
+    name: "Politeknik Negri Jakarta",
+    alamat:
+      "Universitas Indonesia, Jl. Prof. DR. G.A. Siwabessy, Depok City, West Java 16425",
+  },
+];
