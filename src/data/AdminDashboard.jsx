c32155b9b033a711b0dcfeec03605dd527ca6918
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 06:08:30 +0700

    added ManageBuses dashboard

--- src/data/AdminDashboard.jsx
+++ src/data/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import { RiLogoutBoxRFill } from "react-icons/ri";
 import { TbMoodKidFilled } from "react-icons/tb";
 import { FaSchool, FaUserAlt } from "react-icons/fa";
+import { BiSolidBusSchool } from "react-icons/bi";
 
 export const data_dashboard = [
   {
@@ -11,15 +12,21 @@ export const data_dashboard = [
   },
   {
     id: "2",
+    name: "Manage School",
+    link: "/admin/dashboard/school",
+    icon: FaSchool,
+  },
+  {
+    id: "3",
     name: "Manage Driver",
     link: "/admin/dashboard/driver",
     icon: FaUserAlt,
   },
   {
-    id: "3",
-    name: "Manage School",
-    link: "/admin/dashboard/school",
-    icon: FaSchool,
+    id: "4",
+    name: "Manage Buses",
+    link: "/admin/dashboard/buses",
+    icon: BiSolidBusSchool,
   },
   { id: "9", name: "Logout", link: "/", icon: RiLogoutBoxRFill, margin: true },
 ];
