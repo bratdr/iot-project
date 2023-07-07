87d8a1134eac6e5795774c7ef884c326ba34bfda
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 06:09:13 +0700

    added data dummy

--- src/data/Schools.jsx
+++ src/data/Schools.jsx
@@ -15,4 +15,29 @@ export const schools = [
     alamat:
       "Universitas Indonesia, Jl. Prof. DR. G.A. Siwabessy, Depok City, West Java 16425",
   },
+  {
+    id: "4",
+    name: "Universitas Pancasila",
+    alamat: "-",
+  },
+  {
+    id: "5",
+    name: "Universitas Bina Sarana Informasi",
+    alamat: "-",
+  },
+  {
+    id: "6",
+    name: "Politeknik Media Kreatif",
+    alamat: "-",
+  },
+  {
+    id: "7",
+    name: "Universitas Negri Jakarta",
+    alamat: "-",
+  },
+  {
+    id: "8",
+    name: "Universitas Terbuka",
+    alamat: "-",
+  },
 ];
