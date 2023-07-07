69ab41bfab60346743644a4a45a2d86d2ade8326
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 06:11:20 +0700

    update ManageSchool page

--- src/pages/LoginAdmin/ManageSchool/index.jsx
+++ src/pages/LoginAdmin/ManageSchool/index.jsx
@@ -1,8 +1,94 @@
+import { schools } from "../../../data/Schools";
+import Navigation from "../../../components/AdminNav";
+import { useNavigate } from "react-router-dom";
+import { FaSchool } from "react-icons/fa6";
+
 const ManageSchool = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>
-        <h1>ManageSchool</h1>
+      <div className="z-0 flex h-screen w-screen flex-row">
+        <div className="absolute z-10">
+          <Navigation />
+        </div>
+        <div className="flex w-full flex-col items-center justify-center bg-slate-100 pl-20 pt-16 sm:w-full">
+          <div className="w-full overflow-scroll">
+            <div className="mb-4">
+              <h2 className="pb-8 text-4xl font-bold">School Management</h2>
+              <button className="flex flex-row items-center justify-center gap-4 rounded-md bg-black text-sm font-semibold text-white outline outline-1 outline-gray-200 hover:bg-white hover:text-black">
+                <FaSchool size={16} />
+                Add School
+              </button>
+            </div>
+            <div className="overflow-x-scroll overflow-y-scroll border-b border-gray-200 shadow sm:overflow-hidden sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                    >
+                      ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                    >
+                      Alamat
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                    >
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {schools.map((school) => (
+                    <tr key={school.id}>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        <div className="text-sm text-gray-900">{school.id}</div>
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        <div className="text-sm text-gray-900">
+                          {school.name}
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        <div className="text-sm text-gray-900">
+                          {school.alamat}
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        <div className="flex gap-2 text-sm">
+                          <button
+                            onClick={() =>
+                              navigate("/admin/dashboard/school/edit")
+                            }
+                            className="bg-sky-50 text-gray-900 hover:text-sky-800"
+                          >
+                            Edit
+                          </button>
+                          <button className="bg-rose-50 hover:text-red-800">
+                            Delete
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
