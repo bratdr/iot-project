a11e3d07e038805633f93ed702c8def4504989e4
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 06:14:52 +0700

    remove ManageUser view page

--- src/pages/LoginAdmin/ManageUser/Edit/index.jsx
+++ src/pages/LoginAdmin/ManageUser/Edit/index.jsx
@@ -1,5 +1,121 @@
+import { useState, useRef } from "react";
+import { schools } from "../../../../data/Schools";
+import { useNavigate } from "react-router-dom";
+
 const EditUser = () => {
-  return <div>EditUser</div>;
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [parent, setParent] = useState("");
+  const [school, setSchool] = useState("");
+  // const [ktmcard, setKtmCard] = useState("");
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form submission logic
+
+    // Reset form values
+    setName("");
+    setParent("");
+    setSchool("");
+
+    // Clear file input
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+
+    // Show alert window
+    window.alert("Form submitted successfully!");
+  };
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-200 px-8">
+      <div className="flex w-full flex-col rounded bg-slate-800 p-4 sm:w-3/6 xl:w-1/4">
+        <h1 className="text-3xl font-bold text-gray-50">Change User Data</h1>
+      </div>
+      <div className="flex w-full flex-col rounded bg-slate-100 p-4 sm:w-3/6 xl:w-1/4">
+        <form
+          className="flex flex-col gap-4 rounded bg-white p-4 text-sm font-medium outline outline-1 outline-slate-200"
+          onSubmit={handleSubmit}
+          ref={formRef}
+        >
+          <label className="flex flex-col gap-2">
+            Name
+            <input
+              className="rounded p-2 text-sm font-normal outline outline-1 outline-slate-200 focus:bg-sky-50 focus:duration-700"
+              type="text"
+              placeholder="Enter the new name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-2">
+            Parent
+            <input
+              className="rounded p-2 text-sm font-normal outline outline-1 outline-slate-200 focus:bg-sky-50 focus:duration-700"
+              type="text"
+              placeholder="Enter the new parent name"
+              value={parent}
+              onChange={(e) => setParent(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-2">
+            School
+            <div className="relative">
+              <select
+                className="block w-full appearance-none rounded border border-slate-200 bg-white px-4 py-3 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                id="grid-state"
+                value={school}
+                onChange={(e) => setSchool(e.target.value)}
+              >
+                <option value="">Select a school</option>
+                {schools.map((school) => (
+                  <option key={school.id} value={school.name}>
+                    {school.name}
+                  </option>
+                ))}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="h-4 w-4 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </label>
+
+          <div className="flex flex-col gap-2">
+            <label className="flex flex-col">Ktm Card</label>
+            <input
+              className="block w-full cursor-pointer rounded border border-gray-300 bg-gray-50 p-4 text-sm text-gray-900 focus:outline-none"
+              id="default_size"
+              type="file"
+              // onChange={(e) => setKtmCard(e.target.files[0])}
+            />
+            <p className="text-xs text-gray-300">
+              SVG, PNG, JPG or GIF (MAX. 800x400px).
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <input
+              className="rounded bg-sky-400 p-3 text-base font-semibold text-white hover:bg-sky-500 hover:duration-300"
+              type="submit"
+            />
+            <button
+              onClick={() => navigate("/admin/dashboard/user")}
+              className="rounded bg-rose-500 p-3 text-base font-semibold text-white hover:bg-rose-600 hover:duration-300"
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default EditUser;
--- src/pages/LoginAdmin/ManageUser/View/index.jsx
+++
@@ -1,5 +0,0 @@
-const ViewUser = () => {
-  return <div>ViewPage</div>;
-};
-
-export default ViewUser;
