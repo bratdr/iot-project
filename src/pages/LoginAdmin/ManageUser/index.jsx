a488412e28198514b03e098e8219e5d61d7310bd
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 06:12:40 +0700

    update ManageUser page

--- src/pages/LoginAdmin/ManageUser/index.jsx
+++ src/pages/LoginAdmin/ManageUser/index.jsx
@@ -12,16 +12,16 @@ const UsersTable = () => {
         <div className="absolute z-10">
           <Navigation />
         </div>
-        <div className="ml-24 mr-8 flex w-full flex-col items-center justify-center overflow-x-auto px-0 sm:w-full">
-          <div className="w-full">
+        <div className="flex w-full flex-col items-center justify-center bg-slate-100 pl-20 pt-16 sm:w-full">
+          <div className="w-full overflow-scroll">
             <div className="mb-4">
-              <h2 className="mb-8 text-4xl font-bold">Users Management</h2>
+              <h2 className="pb-8 text-4xl font-bold">Users Management</h2>
               <button className="flex flex-row items-center justify-center gap-4 rounded-md bg-black text-sm font-semibold text-white outline outline-1 outline-gray-200 hover:bg-white hover:text-black">
                 <FaUser size={16} />
                 Add User
               </button>
             </div>
-            <div className="overflow-scroll border-b border-gray-200 shadow sm:overflow-hidden sm:rounded-lg">
+            <div className="overflow-x-scroll overflow-y-scroll border-b border-gray-200 shadow sm:overflow-hidden sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
@@ -104,21 +104,13 @@ const UsersTable = () => {
                         <div className="flex gap-2 text-sm">
                           <button
                             onClick={() =>
-                              navigate("/admin/dashboard/user/view")
-                            }
-                            className="bg-slate-100 text-gray-900 hover:text-blue-800"
-                          >
-                            View
-                          </button>
-                          <button
-                            onClick={() =>
                               navigate("/admin/dashboard/user/edit")
                             }
-                            className="bg-sky-300 text-gray-900 hover:text-sky-800"
+                            className="bg-sky-50 text-gray-900 hover:text-sky-800"
                           >
                             Edit
                           </button>
-                          <button className="bg-rose-500 text-white hover:text-red-800">
+                          <button className="bg-rose-50 hover:text-red-800">
                             Delete
                           </button>
                         </div>
