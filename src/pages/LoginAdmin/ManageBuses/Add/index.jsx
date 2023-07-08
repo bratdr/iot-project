dbcb5c765839dfba42cbd7375354b1dfb27eb7f0
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-08 07:51:13 +0700

    added AddBuses page

---
+++ src/pages/LoginAdmin/ManageBuses/Add/index.jsx
@@ -0,0 +1,122 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { buses } from "../../../../data/Buses";
+
+const AddBuses = () => {
+  const navigate = useNavigate();
+  const [nopol, setNopol] = useState("");
+  const [pickup, setPickup] = useState("");
+  const [drop, setDrop] = useState("");
+  const [driver, setDriver] = useState("");
+  // const [ktmcard, setKtmCard] = useState("");
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form submission logic
+
+    // Reset form values
+    setNopol("");
+    setPickup("");
+    setDrop("");
+    setDriver("");
+
+    // Clear file input
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+
+    // Show alert window
+    window.alert("Form submitted successfully!");
+  };
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-200 px-8">
+      <div className="flex w-full flex-col rounded bg-slate-800 p-4 sm:w-3/6 xl:w-1/4">
+        <h1 className="text-3xl font-bold text-gray-50">Add Bus Data</h1>
+      </div>
+      <div className="flex w-full flex-col rounded bg-slate-100 p-4 sm:w-3/6 xl:w-1/4">
+        <form
+          className="flex flex-col gap-4 rounded bg-white p-4 text-sm font-medium outline outline-1 outline-slate-200"
+          onSubmit={handleSubmit}
+          ref={formRef}
+        >
+          <label className="flex flex-col gap-2">
+            Bus Driver
+            <div className="relative">
+              <select
+                className="block w-full appearance-none rounded border border-slate-200 bg-white px-4 py-3 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                id="grid-state"
+                value={driver}
+                onChange={(e) => setDriver(e.target.value)}
+              >
+                <option value="">Select the driver</option>
+                {buses.map((bus) => (
+                  <option key={bus.id} value={bus.driver}>
+                    {bus.driver}
+                  </option>
+                ))}
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="h-4 w-4 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </label>
+
+          <label className="flex flex-col gap-2">
+            Police Number
+            <input
+              className="rounded p-2 text-sm font-normal outline outline-1 outline-slate-200 focus:bg-sky-50 focus:duration-700"
+              type="text"
+              placeholder="Enter the bus police number"
+              value={nopol}
+              onChange={(e) => setNopol(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-2">
+            Pickup Location
+            <input
+              className="rounded p-2 text-sm font-normal outline outline-1 outline-slate-200 focus:bg-sky-50 focus:duration-700"
+              type="text"
+              placeholder="Enter the bus first pickup location"
+              value={pickup}
+              onChange={(e) => setPickup(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-2">
+            Drop Location
+            <input
+              className="rounded p-2 text-sm font-normal outline outline-1 outline-slate-200 focus:bg-sky-50 focus:duration-700"
+              type="text"
+              placeholder="Enter the bus last stop location"
+              value={drop}
+              onChange={(e) => setDrop(e.target.value)}
+            />
+          </label>
+
+          <div className="flex flex-col gap-2">
+            <input
+              className="rounded bg-sky-400 p-3 text-base font-semibold text-white hover:bg-sky-500 hover:duration-300"
+              type="submit"
+            />
+            <button
+              onClick={() => navigate("/admin/dashboard/buses")}
+              className="rounded bg-rose-500 p-3 text-base font-semibold text-white hover:bg-rose-600 hover:duration-300"
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddBuses;
