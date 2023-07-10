ee099477141549a88eb90d39d35e9bc63e9e20d1
Brata Darmawan <bratdr@Bratas-iMac.local>
2023-07-11 04:05:22 +0700

    change button with UserSearch components

--- src/pages/LoginUser/index.jsx
+++ src/pages/LoginUser/index.jsx
@@ -1,17 +1,7 @@
-import { useNavigate } from "react-router-dom";
 import trackingGif from "../../assets/tracking.gif";
+import UserSearch from "../../components/UserSearch";
 
 const UserLogin = () => {
-  const navigate = useNavigate();
-
-  const handleLoginButtonClick = () => {
-    navigate("/login/user/keys");
-  };
-
-  const handleBackButtonClick = () => {
-    navigate("/");
-  };
-
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="text-center text-3xl font-bold">
@@ -21,20 +11,7 @@ const UserLogin = () => {
       <p className="pb-6">Insert Your Childern Tracking Code!</p>
       <img className="max-w-xs" src={trackingGif} alt="my-gif" />
       <div className="flex flex-col items-center justify-center gap-6 pt-6">
-        <div className="flex flex-row items-center justify-center gap-2">
-          <button
-            onClick={handleLoginButtonClick}
-            className="mt-12 flex w-24 items-center justify-center rounded-lg bg-black text-sm font-medium text-white hover:bg-rose-500 hover:font-bold hover:text-white"
-          >
-            Login
-          </button>
-          <button
-            onClick={handleBackButtonClick}
-            className="mt-12 flex w-24 items-center justify-center rounded-lg border border-black bg-transparent text-sm font-medium text-black hover:bg-rose-500 hover:font-bold hover:text-white"
-          >
-            Back
-          </button>
-        </div>
+        <UserSearch />
       </div>
     </div>
   );
